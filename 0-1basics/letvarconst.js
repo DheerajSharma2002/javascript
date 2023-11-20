3b8558fb3242d95195bb1e3ae18f41a4529f5300
dheeraj <ds817897@gmail.com>
2023-11-20 22:33:58 +0530

    let var cons initialize

---
+++ 0-1basics/letvarconst.js
@@ -0,0 +1,13 @@
+const accountId = 14541
+let accountEmail = "dheeraj@google.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
